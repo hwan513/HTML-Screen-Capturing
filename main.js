bff0b4b1d2898192b8d1f61a01d11d1aa2e85802
Henry Wang <henrywang@Henrys-MacBook-Pro-3.local>
2023-07-26 14:20:51 +1200

    feat: initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+/* ES5 */
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
+import { saveAs } from "file-saver";
+import html2canvas from "html2canvas";
+
+function handleClick() {
+  // var captureElement = document.getElementById("hero");
+  var captureElement = document.body;
+  // alert("hi");
+
+  // html_to_image_append();
+  html_to_image_download();
+  // html2canvas_append();
+  // html2canvas_download();
+
+  function html_to_image_append() {
+    htmlToImage
+      .toPng(captureElement)
+      .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
+  }
+
+  function html_to_image_download() {
+    htmlToImage.toPng(captureElement).then(function (dataUrl) {
+      saveAs(dataUrl, "html-to-image.png");
+    });
+  }
+
+  function html2canvas_append() {
+    html2canvas(captureElement).then(function (canvas) {
+      document.body.appendChild(canvas);
+    });
+  }
+
+  function html2canvas_download() {
+    html2canvas(captureElement).then(function (canvas) {
+      const image = canvas.toDataURL("image/png", 1.0);
+
+      canvas.toBlob(function (blob) {
+        saveAs(blob, "html2canvas.png");
+      });
+    });
+  }
+}
+
+captureButton = document.getElementById("captureButton");
+captureButton.addEventListener("click", handleClick);
